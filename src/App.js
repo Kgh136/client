60b14ee5fd6fafc77b6dd9581e319823da7d28f2
Kim Michael Garcia Haas <Kgh13690@gmail.com>
2022-03-08 13:42:23 +0100

    Databaser

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { get, post, put, taBort } from "./utility/api";
 
 function App() {
+  const [id, setId] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <input
+          value={id}
+          onChange={(event) => setId(event.target.value)}
+        ></input>
+        <button
+          onClick={() => {
+            get("/api/read").then((response) => setTodos(response.data));
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          READ
+        </button>
+        <button
+          onClick={() => {
+            post("/api/create", {
+              id: counter,
+              title: "test " + counter,
+              description: "Lorem ipsum " + counter,
+            }).then((response) => console.log(response));
+            setCounter((previous) => previous + 1);
+          }}
+        >
+          CREATE
+        </button>
+        <button
+          onClick={() => {
+            put(`/api/update/${id}`, {
+              title: "Updated!",
+              description: "Updated!",
+            }).then((response) => console.log(response));
+          }}
+        >
+          UPDATE
+        </button>
+        <button
+          onClick={() => {
+            taBort(`/api/delete/${id}`);
+          }}
+        >
+          DELETE
+        </button>
+      </div>
+      <div>
+        <ul>
+          {todos.map((todo) => {
+            return <li key={todo.id}>{todo.title}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
